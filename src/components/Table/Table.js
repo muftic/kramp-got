9f41586a2d8f6c9183eaa823b22989060670f8ac
muftic <marijan.muftic@gmail.com>
2022-04-06 00:33:46 +0200

    Table cleanup, css paginator hide

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,45 +1,16 @@
 import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { cardActionAreaClasses } from "@mui/material";
 import { columns } from "../../config/constants";
 export default function Table(props) {
-  let myRows = props.characters.map((char, i) => {
-    return {
-      id: i + 1 + (props.currentPage - 1) * 10,
-      age: char.age,
-      gender: char.gender,
-      aliases: char.aliases,
-      allegiances: char.allegiances,
-      books: char.books,
-      born: char.born,
-      culture: char.culture,
-      died: char.died,
-      father: char.father,
-      mother: char.mother,
-      name: char.name,
-      playedBy: char.playedBy,
-      povBooks: char.povBooks,
-      spouse: char.spouse,
-      titles: char.titles,
-      tvSeries: char.tvSeries,
-      url: char.url,
-    };
-  });
-
-  useEffect(() => {}, []);
-
   return (
     <div style={{ height: "60vw", width: "100%" }}>
       <DataGrid
-        // rowsPerPageOptions="10"
-        rows={myRows}
+        //  loading={props.isLoading}
+
+        autoHeight
+        pageSize={10}
+        rows={props.rows}
         columns={columns}
-        /*   pageSize={10}
-   
-        disableSelectionOnClick={false}
-        paginationMode="client" */
       />
     </div>
   );
